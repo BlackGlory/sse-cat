5fffb87b328c38eac297e528af0ca64e0d237eed
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-10 02:12:51 +0800

    chore: upgrade dependencies

--- src/from-multiple-server-sent-events.ts
+++ src/from-multiple-server-sent-events.ts
@@ -3,5 +3,5 @@ import { fromServerSentEvent } from './from-server-sent-event'
 
 export function fromMultipleServerSentEvents(urls: string[], { events, headers }: { events: string[], headers: Headers }): Observable<string> {
   const observables = urls.map(url => fromServerSentEvent(url, { events, headers }))
-  return merge<string>(...observables)
+  return merge(...observables)
 }
